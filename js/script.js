9b8f5184a5469c6ea5c866e4c9a65d8508048818
Byunwoojin <sforot9@gmail.com>
2021-04-02 10:01:53 +0900

    04.02

--- js/script.js
+++ js/script.js
@@ -1,44 +1,42 @@
-
-/* 채팅횟수 그래프 data*/
-/*
-let fin_data;
-fetch("http://www.afyproject2.ml/analyze")
-    .then((response)=> response.json())
-    .then((data)=>fin_data=data.data);
-
-
-let labels1 = fin_data.participant_list;
-let datas1=fin_data.participant_chat.map(function (item) {
-    for (let i = 0; i < item.length; i++) {
-       return item[1];
-    }
-});
-
 /*카카오 분석일
+ {movies.map(movie => (
+                            <Movie
+                                key={movie.id}
+                                id={movie.id}
+                                year={movie.year}
+                                title={movie.title}
+                                summary={movie.summary}
+                                poster={movie.medium_cover_image}
+                                genres={movie.genres}
+                                */
 
-let start_date=fin_data.date_data.start;
-let end_date=fin_data.date_data.end;
 
-let now_date=document.getElementById("now_date");
-let duration=document.getElementById("duration");
-let total_chat=document.getElementById("total_chat");
-let average_chat=document.getElementById("average_chat");
 
-now_date.value=Date.now();
-duration.innerHTML="분석기간 : "+start_date+" ~ "+end_date;
-*/
-/* 서버에서 사용할 때는 2줄 지워주시면 됩니다.*/
+const fs=require('fs');
+var filename ='https://www.afyproject2.ml//home/ubuntu/kusitms_companyPJ/routes/analyze_result.json';
+
+var analyze_result_file = readFileSync(filename, 'utf8');
+  analyze_result_string = analyze_result_file.toString();
+  analyze_result = JSON.parse(analyze_result_string);
+
+
 document.getElementById("now_date").innerHTML="분석일자 :"+new Date().toLocaleString();
-let start_date="2021.02.03";
-document.getElementById("duration").innerHTML="분석기간 :"+start_date;
-let total_chat="1000";
+let start_date=analyze_result.data.date_data.start;
+let end_date=analyze_result.data.date_data.end;
+document.getElementById("duration").innerHTML="분석기간 :"+start_date+"~"+end_date;
+
+let participant=Object.values(analyze_result.data.participant_list);
+document.getElementById("participant").innerHTML="단톡방 인원: "+participant.length+"개";
+
+let chat=Object.values(fin_data.data.participant_chat.Chat_counts); /*각자 채팅 횟수*/
+let total_chat=0;
+chat.forEach(function(item){total_chat+=Number(item);})
 document.getElementById("total_chat").innerHTML="전체 채팅횟수: "+total_chat+"개";
-let average_chat='50';
-document.getElementById("average_chat").innerHTML="하루평균 채팅횟수: "+average_chat+"개";
 
-let labels1=['1','2','3','4','5','6'];
-let datas1=[10,20,30,40,50,60];
 
+/* 3번째: 채팅 빈도수 그래프 */
+let labels1=participant; 
+let datas1=Object.values(fin_data.data.participant_chat.Chat_counts); /*각자 채팅 횟수*/
 function getRandomColor() {
     let colors=new Array();
     for(let i=0;i<labels1.length;i++){
@@ -74,38 +72,50 @@ let chart1 = new Chart(myChart1,{
     }
 
 });
-/*오각형 그래프 */
+/*4번째: 오각형 그래프 */
 let labels2 = ['근로성','협조성','연관성','적극성','성실성'];
+function getData(index){
+    datasets: [{
+        label: participant[0],
+        fill:true,
+        backgroundColor:getRandomColor(),
+        poinBoderColor:getRandomColor(),
+        pointBorder:getRandomColor(),
+        pointBorderColor:"#fff",
+        pointBackgroundColor:"#ddd",
+        data:
+            analyze_result.relation.User[participant_list[0]],
+            analyze_result.data.workabillity.User[participant_list[0]],
+            analyze_result.data.participant.User[participant_list[0]],
+            analyze_result.participant_activity.Activity[participant_list[0]]
+    
+}]
+    return datasets;
+};
 let myChart2 = document.getElementById("myChart2").getContext('2d');
 let chart2 = new Chart(myChart2,{
     type: 'radar',
-    data: {
-        labels: labels2,
-        /*
-        for(let i=0;i<labels2.length;i++){
-            let key = keyArr[i]
-            let datas[key]=personArr[i]
-            */
-        datasets:[{
+    data:
+        /*datasets:[
+            {
             label: "A Person",
             fill:true,
-            /* backgroundColor 랜덤, borderColor 랜덤, pintBackgroudn*/
-            backgroundColor: "rgba(255, 52, 105,0.1)",
-            boarderColor:"rgba(179,181,198,1)",
+            backgroundColor: "rgba(255, 170, 113,0.2)",
+            boarderColor:"rgba(100,90,198,1)",
             pointBorderColor:"#fff",
-            pointBackgroundColor:"#aaaccc",
-            data:[90,60,42,37,81] 
+            pointBackgroundColor:"rgba(70,190,198,1.2)",
+            data:[17,82,50,14,27] 
         },
        {
             label: "B Person",
             fill:true,
-            backgroundColor: "rgba(255, 170, 113,0.2)",
+            backgroundColor: "rgba(255, 99, 113,0.2)",
             boarderColor:"rgba(70,190,198,1)",
             pointBorderColor:"#fff",
             pointBackgroundColor:"rgba(70,190,198,1.2)",
             data:[17,82,50,14,27] 
         }
-        ] 
+        ] */
     },
     options:{
         responsive:false,
@@ -121,7 +131,7 @@ let chart2 = new Chart(myChart2,{
 });
 
 /*채팅 길이 그래프(1)*/
-let labels3 = ['1','2','3','4','5','6'];
+let labels3 = participant;
 let datas3 = [190,130,120,140,170,80];
 let colors3=getRandomColor();
 let myChart3 = document.getElementById("myChart3").getContext('2d');
